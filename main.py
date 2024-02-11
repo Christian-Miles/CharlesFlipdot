1df64f35525560b1f518491bff9feaac94af5f3d
Christian Miles <Christian-Miles@users.noreply.github.com>
2024-02-11 23:37:46 +0000

    SRS and pseudocode

---
+++ main.py
@@ -0,0 +1,33 @@
+#import machine
+#import rp2
+#import ulab
+
+# REQS
+"""
+1. Local time via WIFI
+2. Display Hours, Minutes, and Seconds (pending condition 3)
+3. If time between 10pm -> 5am, dont display seconds
+4. If time between 5am -> 10pm, follow these conditions:
+    a. IF on the 5 minute mark (but not on the hour): using ADSB (or some web based tool), display the local closest flight, its number, its from/to, and its altitude/airspeed
+    b. IF on the hour, display paman, game of life, space invaders, or random
+"""
+
+#PSEUDO
+"""
+function ADSB():
+    pass
+    
+function hourmark():
+    pass
+    
+IF time.minute % 5 AND time.minute != 0:
+    display ADSB
+ELSEIF time.minute == 0:
+    display hourmark()
+ELSE:
+    IF time % 5:
+        display normaltime
+    ELSE
+        display shortenedtime
+    
+"""
